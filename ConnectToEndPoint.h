a974be1ea717e4ca1cd75baf45a32f0e955686fc
Carlo Wood <carlo@alinoe.com>
2022-06-09 18:57:58 +0200

    Fix typo in comments.

--- ConnectToEndPoint.h
+++ ConnectToEndPoint.h
@@ -95,7 +95,7 @@ class ConnectToEndPoint : public AIStatefulTask
   /// Attempt a connect to address. Returning false is a failure, otherwise this function should result in a single call to connect_result.
   bool connect(evio::SocketAddress const& address);
 
-  /// Implemenation of state_str for run states.
+  /// Implementation of state_str for run states.
   char const* state_str_impl(state_type run_state) const override;
 
   /// Handle mRunState.
